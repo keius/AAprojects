e1b9406b811d8ca01482aca395fa3014a29c9c0c
Greg <waterparkmelon@gmail.com>
2017-06-22 23:18:43 -0700

    finished album partial

--- MusicApp/config/routes.rb
+++ MusicApp/config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   root to: redirect("/bands")
 
   resources :bands do
-    resources :albums, only: [:new] do
-      resources :tracks, only: [:new]
-    end
+    resources :albums, only: [:new]
   end
 
-  resources :albums, only: [:create, :show, :destroy, :edit, :update]
+  resources :albums, only: [:create, :show, :destroy, :edit, :update] do
+    resources :tracks, only: [:new]
+  end
+
   resources :users, only: [:new, :create, :show]
+  
   resources :tracks, only: [:create, :show, :destroy, :edit, :update]
 
   resource :session, only: [:new, :create, :destroy]
